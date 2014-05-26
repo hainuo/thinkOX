fe74a49e25e2968d2016d182f3ee6798638f8be2
hainuo <admin@hainuo.info>
2014-05-26 23:03:52 +0800

    修正上传文件大小的地方

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -35,7 +35,7 @@ return array(
     /* 图片上传相关配置 */
     'PICTURE_UPLOAD' => array(
 		'mimes'    => '', //允许上传的文件MiMe类型
-		'maxSize'  => 2*1024*1024, //上传的文件大小限制 (0-不做限制)
+		'maxSize'  => 4*1024*1024, //上传的文件大小限制 (0-不做限制)
 		'exts'     => 'jpg,gif,png,jpeg', //允许上传的文件后缀
 		'autoSub'  => true, //自动子目录保存文件
 		'subName'  => array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
